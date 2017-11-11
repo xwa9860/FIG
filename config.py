62444c580fe24c70c067a875fec99118bf16425a
Xin Wang <imwangxin@gmail.com>
2017-11-11 10:08:18 -0800

    correct because all fuel material files are in the fuel_mat folder now

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ FLUX_ACT_AVE_FOLDER =           'fuel_mat/fuel_comp/flux_act_ave_serp/'
 FLUX_ALL_AVE_FOLDER =           'fuel_mat/fuel_comp/flux_all_ave_serp/'
 FRESH_FILE =                    'fuel_mat/fuel_comp/fresh'
 MCNP_RAW_FOLDER =               'fuel_mat/fuel_comp/raw_mcnp/'# 160 raw mcnp material compositions from mark1.txt
-OUTPUT_FUEL_FOLDER =            'FUEL_COMP'
+OUTPUT_FUEL_FOLDER =            'fuel_mat/fuel_comp'
